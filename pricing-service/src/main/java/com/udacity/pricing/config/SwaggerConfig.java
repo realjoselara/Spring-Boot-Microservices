716978f824e7665eb0cc9fb522b8f06bb16802c6
realjoselara <joselara02@outlook.com>
2021-01-09 23:35:33 -0500

    added Swagger UI to Document API

---
+++ pricing-service/src/main/java/com/udacity/pricing/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.udacity.pricing.config;
+
+
+import io.swagger.annotations.ApiResponses;
+import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+@ApiResponses(value = {
+
+})
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Pricing API",
+                "This API returns the price for a particular vehicle.",
+                "1.0",
+                "http://www.joselara.me",
+                new Contact("Jose Lara", "www.joselara.me", "latinocodes02@gmail.com"),
+                "License of API", "https://www.joselara.me", Collections.emptyList());
+    }
+
+}
